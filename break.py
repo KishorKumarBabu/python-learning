8c2a01b40135b655e32062d57344b8bff4e345a6
kishor kumar <kishorkumarbabu5@gmail.com>
2024-02-27 18:16:18 +0530

    "break statement"

---
+++ break.py
@@ -0,0 +1,10 @@
+x=int(input("enther how may candy"))
+avg=10
+i=1
+while(i<=x):
+    if(i==avg):
+        print("sorry we have only",avg,"candy")
+        break
+    print("candy")
+    i=i+1
+    
