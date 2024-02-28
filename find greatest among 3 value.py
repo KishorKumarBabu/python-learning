e05b7ffb91f76a9af3e28f93e14b21ff2276dcc9
kishor kumar <kishorkumarbabu5@gmail.com>
2024-02-28 20:13:26 +0530

    "assignment"

---
+++ find greatest among 3 value.py
@@ -0,0 +1,11 @@
+a=int(input("1:"))
+b=int(input("2:"))
+c=int(input("3:"))
+if a>b>c:
+    print("c is greater")
+elif a<b<c :
+    print("a is greater")
+elif a<b>c :
+    print("b is greater")
+else:
+    print("enter valide input")
