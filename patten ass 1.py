848c96425dad7b79abe0f737411b5816a09ef75c
kishor kumar <kishorkumarbabu5@gmail.com>
2024-02-29 17:34:48 +0530

    "patten assignment"

--- patten ass 1.py
+++ patten ass 1.py
@@ -1,4 +1,8 @@
-for i in range(1,5):
-    for j in range(i,5):
-        print(j,end=" ")
-    print()
+a="ABCD"
+b="PQR"
+for i in range(4):
+    for j in range(3):
+        print(a[0:i+1],end="")
+        print(b[i:j+3],end="")
+        break
+    print() 
