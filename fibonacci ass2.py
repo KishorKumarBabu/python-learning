f0c905947b917753aeba078189391259af149275
kishor kumar <kishorkumarbabu5@gmail.com>
2024-03-14 18:55:30 +0530

    "assignment"

--- fibonacci ass2.py
+++ fibonacci ass2.py
@@ -2,12 +2,16 @@ x=int(input("enter tn num"))
 def fib(n):
     a=0
     b=1
-    for i in range(n):
-        print(a,end=" ")
-        sum=a+b
-        a=b
-        b=sum
-        sum=a+b
+    if(x<0):
+        print("invalid")
+    else:   
+        for i in range(n):
+            if(a<=n):
+                print(a,end=" ")
+                sum=a+b
+                a=b
+                b=sum
+                
 fib(x)
 
     
