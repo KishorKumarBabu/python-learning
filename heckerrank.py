91f311696baeb911082cca538f5ee5d3a4066128
kishor kumar <kishorkumarbabu5@gmail.com>
2024-03-14 21:22:11 +0530

    "heacker"

--- heckerrank.py
+++ heckerrank.py
@@ -11,3 +11,25 @@ a = int(input())
 b = int(input())
 print(a//b)
 print(a/b)
+
+#sqrt
+n = int(input())
+for i in range(n):
+    a=i*i
+    print(a)
+    
+#leap
+def is_leap(year):
+    if year==2100:
+        leap=False
+    elif year%4==0:
+        leap = True
+    elif year%4==0:
+        leap=False
+    else:
+        leap=False
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
