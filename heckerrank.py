9e3c7ff32f29c03b7f148ed9e5ab0888d80ad3c6
kishor kumar <kishorkumarbabu5@gmail.com>
2024-03-17 10:21:42 +0530

    "heackerrank"

--- heckerrank.py
+++ heckerrank.py
@@ -38,3 +38,22 @@ print(is_leap(year))
 n = int(input())
 for i in range(1,n+1):
     print(i,end="")
+    
+#list comparision
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    i1=[_ for _ in range(x+1)]
+    j1=[_ for _ in range(y+1)]
+    k1=[_ for _ in range(z+1)]
+    l=[]
+    for i in i1:
+        for j in j1:
+            for k in k1:
+                if(i+j+k !=n):
+                    l.append([i,j,k])
+                    
+print(l)        
