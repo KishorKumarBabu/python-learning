640b8ae42d76098a540867630eadbf49287005af
kishor kumar <kishorkumarbabu5@gmail.com>
2024-04-01 20:54:22 +0530

    "heackerrank"

--- heckerrank.py
+++ heckerrank.py
@@ -89,4 +89,27 @@ if __name__ == '__main__':
         if arr[i] > arr[i+1]:
             print(arr[i+1])
             break
-            a  
+
+
+
+# list
+
+if __name__ == '__main__':
+    N = int(input())
+    lst=[]
+    for item in range(N):
+        cmd=input().split()
+        if cmd[0]=='insert':
+            lst.insert(int(cmd[1]),int(cmd[2]))
+        if cmd[0]=='print':
+            print(lst)
+        if cmd[0]=='remove':
+            lst.remove(int(cmd[1]))
+        if cmd[0]=='append':
+            lst.append(int(cmd[1]))
+        if cmd[0]=='sort':
+            lst.sort()
+        if cmd[0]=='pop':
+            lst.pop()
+        if cmd[0]=='reverse':
+            lst.reverse()
