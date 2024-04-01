79ead18637be8ac4f611772dfac155d9399dcb0b
kishor kumar <kishorkumarbabu5@gmail.com>
2024-04-01 18:00:42 +0530

    ""

---
+++ comparing two object.py
@@ -0,0 +1,19 @@
+class computer:
+    def __init__(self):
+        self.name="kishor"
+        self.age=20
+    def update(self):
+        self.age=18
+    def compare(self,other):
+        if(self.age==other.age):
+            return True
+        else:
+            return False
+c1=computer()
+c2=computer()
+if c1.compare(c2):
+    print("same")
+else:
+    print("different")
+print(c1.age)
+print(c2.age)
