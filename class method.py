df0fb976b17586a860e33221ee58dbfeb11bbb55
kishor kumar <kishorkumarbabu5@gmail.com>
2024-04-02 18:38:51 +0530

    "class method inc&o"

---
+++ class method.py
@@ -0,0 +1,16 @@
+class student:
+    school="kishor"
+    def __init__(self,m1,m2,m3):
+        self.m1=m1
+        self.m2=m2
+        self.m3=m3
+    def avg(self):
+        return (self.m1+self.m2+self.m3)/3
+    @classmethod
+    def getschool(cls):
+        return cls.school
+s1=student(50,98,45)
+s2=student(98,54,67)
+print(s1.avg())
+print(s2.avg())
+print(student.getschool())
