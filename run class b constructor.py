f699dc914bdeb3f875fdade768285f4028d85153
kishor kumar <kishorkumarbabu5@gmail.com>
2024-04-03 20:29:39 +0530

    "run class b constructor"

---
+++ run class b constructor.py
@@ -0,0 +1,15 @@
+class a:
+    def __init__(self):
+        print("in a init")
+    def feature1(self):
+        print("feature 1 is working")
+    def feature2(self):
+        print("feature 2 is working")
+class b(a):
+    def __init__(self):
+        print("in b init")
+    def feature3(self):
+        print("feature 3 is working")
+    def feature4(self):
+        print("feature 4 is working")
+b1=b()
