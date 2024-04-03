9cdeaea014cf5938cb963d2f0dfa45b67f7237a4
kishor kumar <kishorkumarbabu5@gmail.com>
2024-04-03 18:11:18 +0530

    "single level inheritance"

---
+++ single level inheritance.py
@@ -0,0 +1,12 @@
+class a:
+    def feature1(self):
+        print("feature 1 is working")
+    def feature2(self):
+        print("feature 2 is working")
+class b(a):
+    def feature3(self):
+        print("feature 3 is working")
+    def feature4(self):
+        print("feature 4 is working")
+b1=b()
+b1.feature1()
