f7039db9f37ee6a0617c29cf753c9499f938c830
kishor kumar <kishorkumarbabu5@gmail.com>
2024-04-05 20:30:39 +0530

    "duck typing"

---
+++ duck typing.py
@@ -0,0 +1,13 @@
+class vscode:
+    def execute(self):
+        print("compailing")
+        print("runing")
+class myedtior:
+    def execute(self):
+        print("spell check")
+class laptop:
+    def code(self,ide):
+        ide.execute()
+ide=vscode()
+lap1=laptop()
+lap1.code(ide)
