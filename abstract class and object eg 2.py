6f631ce338759abd80123be33798525aba24a35b
kishor kumar <kishorkumarbabu5@gmail.com>
2024-04-09 10:42:05 +0530

    "# if we create a class laptop it as no method and it inherits computer so it access the process of computer so it is a abstract class so we cannot able to create a object it show an error"

---
+++ abstract class and object eg 2.py
@@ -0,0 +1,12 @@
+from abc import ABC,abstractmethod
+class computer(ABC):
+    @abstractmethod
+    def process(self):
+        pass
+class laptop(computer):
+    pass
+com=computer()
+lap=laptop()
+lap.process()
+
+# if we create a class laptop it as no method and it inherits computer so it access the process of computer so it is a abstract class so we cannot able to create a object it show an error
