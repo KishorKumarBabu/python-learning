20753cee70990eeeff4a506d5f19115dac613532
kishor kumar <kishorkumarbabu5@gmail.com>
2024-04-24 19:38:37 +0530

    "sql connection"

--- example sqlconnection.py
+++ example sqlconnection.py
@@ -1,3 +1,3 @@
 import mysql.connector
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="1234")
+mydb=mysql.connector.connect(host="localhost",user="root",passwd="1234",database="kishorkumar")
 
