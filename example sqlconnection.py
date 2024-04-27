190ea1bdaa8b6871b505bfa201cd9f119b72abec
kishor kumar <kishorkumarbabu5@gmail.com>
2024-04-27 18:01:45 +0530

    "sqk connction"

--- example sqlconnection.py
+++ example sqlconnection.py
@@ -1,3 +1,3 @@
 import mysql.connector
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="1234",database="kishorkumar")
+mydb=mysql.connector.connect(host="localhost",user="root",passwd="1234",database="kishor")
 
