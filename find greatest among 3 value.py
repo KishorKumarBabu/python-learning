43ca81ddc801e9ef8508dc979ed546a736eacd6c
kishor kumar <kishorkumarbabu5@gmail.com>
2024-07-07 07:43:14 +0530

    234567

--- find greatest among 3 value.py
+++ find greatest among 3 value.py
@@ -2,9 +2,9 @@ a=int(input("1:"))
 b=int(input("2:"))
 c=int(input("3:"))
 if a>b>c:
-    print("c is greater")
-elif a<b<c :
     print("a is greater")
+elif a<b<c :
+    print("c is greater")
 elif a<b>c :
     print("b is greater")
 else:
